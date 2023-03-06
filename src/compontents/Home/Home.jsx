25fb599b76ec6873e6d935ba2e47b88cbd24e691
FelipeSiravegna <felipesiravegna1@gmail.com>
2023-03-06 16:18:41 -0300

    Inicio page wip

--- src/compontents/Home/Home.jsx
+++ src/compontents/Home/Home.jsx
@@ -3,7 +3,10 @@ import style from './Home.module.css'
 export default function Home() {
     return (
         <div className={style.container}>
-            <h1>Home</h1>
+            <div className={style.info}>
+                <h3 className={style.saludo}>¡Hola!</h3>
+                <h3 className={style.presentacion}>Soy Felipe Siravegna, full stack developer graduado del bootcamp de SoyHenry 🚀</h3>
+            </div>
         </div>
     )
 }
