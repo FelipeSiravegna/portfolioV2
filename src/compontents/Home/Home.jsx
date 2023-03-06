a021c978ca9867e1f3b958388a96d6c0c277ab37
FelipeSiravegna <felipesiravegna1@gmail.com>
2023-03-06 15:35:07 -0300

    navBar items and styles done

--- src/compontents/Home/Home.jsx
+++ src/compontents/Home/Home.jsx
@@ -1,5 +1,9 @@
+import NavBar from "../NavBar/NavBar"
+
 export default function Home() {
     return (
-        <h1>Bienvenidos a mi portfolio</h1>
+        <div>
+            <NavBar />
+        </div>
     )
 }
---
+++ src/compontents/NavBar/NavBar.jsx
@@ -0,0 +1,13 @@
+import style from './NavBar.module.css'
+
+export default function NavBar() {
+    return (
+        <div className={style.container}>
+            <a href="#inicio" className={style.navBarItem}><h3>Inicio</h3></a>
+            <a href="#sobreMi" className={style.navBarItem}><h3>Sobre mí</h3></a>
+            <a href="#conocimientos" className={style.navBarItem}><h3>Conocimientos</h3></a>
+            <a href="#proyectos" className={style.navBarItem}><h3>Proyectos</h3></a>
+            <a href="#contactame" className={style.navBarItem}><h3>Contactame</h3></a>
+        </div>
+    )
+}
