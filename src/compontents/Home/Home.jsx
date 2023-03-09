533f3a6cfd1ee0c09bdef3b4216b6e6dc6fdabfa
FelipeSiravegna <felipesiravegna1@gmail.com>
2023-03-09 16:49:08 -0300

    Welcome page done

--- src/compontents/Home/Home.jsx
+++ src/compontents/Home/Home.jsx
@@ -1,11 +1,30 @@
 import style from './Home.module.css'
+import PROFILE from '../../media/profile.jpg'
+import CV from '../../media/curriculum.svg'
+import GITHUB from '../../media/github.svg'
+import LINKEDIN from '../../media/linkedin.svg'
+import RESUME from '../../files/Resume.pdf'
 
 export default function Home() {
     return (
         <div className={style.container}>
             <div className={style.info}>
-                <h3 className={style.saludo}>¡Hola!</h3>
-                <h3 className={style.presentacion}>Soy Felipe Siravegna, full stack developer graduado del bootcamp de SoyHenry 🚀</h3>
+                <img src={PROFILE} alt="PROFILE" className={style.profile} />
+                <div className={style.texto}>
+                    <h3 className={style.saludo}>¡Hola!</h3>
+                    <h3 className={style.presentacion}>Mi nombre es <b className={style.yellow}>Felipe Siravegna</b> y soy <b className={style.yellow}>Desarrollador Web Full Stack</b></h3>
+                    <div className={style.clickableImages}>
+                        <a href={RESUME} download="Felipe Siravegna CV">
+                            <img src={CV} alt="CV" className={style.infoImages} />
+                        </a>
+                        <a href='https://github.com/FelipeSiravegna' target='_blank'>
+                            <img src={GITHUB} alt="GITHUB" className={style.infoImages} />
+                        </a>
+                        <a href='https://www.linkedin.com/in/felipe-siravegna/' target='_blank'>
+                            <img src={LINKEDIN} alt="LINKEDIN" className={style.infoImages} />
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
     )
