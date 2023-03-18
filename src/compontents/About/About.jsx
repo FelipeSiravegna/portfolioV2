ff02f516cea0fe9515b6d612b3c66745584d1b10
Felipe Siravegna <felipeisiravegna1@gmail.com>
2023-03-18 10:18:29 -0300

    ssh test

--- src/compontents/About/About.jsx
+++ src/compontents/About/About.jsx
@@ -3,7 +3,7 @@ import style from './About.module.css'
 export default function About() {
     return (
         <div className={style.container}>
-            <h1>Sobre mí</h1>
+            <h1>Sobre mí!</h1>
         </div>
     )
 }
